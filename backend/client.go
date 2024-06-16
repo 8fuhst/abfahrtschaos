96c52e8fe3f65e96d6d28a4d66b1f9c044c48bdc
8fuhst <59779937+8fuhst@users.noreply.github.com>
2024-06-16 15:24:57 +0200

    Authentication works

--- backend/client.go
+++ backend/client.go
@@ -15,8 +15,22 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type departure_list_body struct {
-	Station string `json:"station"`
+type cnRequest struct {
+	TheName     sdName `json:"theName"`
+	MaxList     int    `json:"maxList"`
+	MaxDistance int    `json:"maxDistance"`
+}
+
+type dlRequest struct {
+	Station       sdName `json:station`
+	Time          string `json:time`
+	MaxList       int    `json:maxList`
+	MaxTimeOffset int    `json:maxTimeOffset`
+}
+
+type sdName struct {
+	Name string `json:"name"`
+	City string `json:"city"`
 }
 
 func getEnvVariable(key string) string {
@@ -29,14 +43,15 @@ func getEnvVariable(key string) string {
 	return os.Getenv(key)
 }
 
-func getSignature(request_body []byte) string {
-	pass := os.Getenv("API_PASS")
+func getSignature(requestBody []byte) string {
+	pass := getEnvVariable("API_PASS")
+	fmt.Printf("Password: %s\n", pass)
 	pass_bytes := []byte(pass) //key
 
 	//bytes_to_hash := append(pass_bytes, request_body...)
 	//hashed := hmac.New(sha1.New, bytes_to_hash)
 	hashed := hmac.New(sha1.New, pass_bytes)
-	_, err := hashed.Write(request_body)
+	_, err := hashed.Write(requestBody)
 	if err != nil {
 		log.Fatalf("Error during signature generation")
 	}
@@ -44,25 +59,8 @@ func getSignature(request_body []byte) string {
 	return signature
 }
 
-func main() {
+func execRequest(client *http.Client, signature string, req *http.Request) *http.Response {
 	username := getEnvVariable("API_USERNAME")
-	fmt.Print("Starting HTTP Client...")
-	client := &http.Client{}
-
-	d := departure_list_body{"Altona"}
-	body, err := json.Marshal(d)
-	if err != nil {
-		log.Fatalf("Error while creating JSON")
-	}
-	body_buffer := bytes.NewBuffer(body)
-	req, err := http.NewRequest("POST", "http://gti.geofox.de/gti/public/departureList", body_buffer)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	signature := getSignature(body)
-	fmt.Print(signature)
 
 	req.Header.Add("geofox-auth-user", username)
 	req.Header.Add("geofox-auth-signature", signature)
@@ -76,6 +74,63 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	return resp
+}
+
+func requestCheckName(stationName string, cityName string, maxList int, maxDistance int, client *http.Client) *http.Response {
+	url := "http://gti.geofox.de/gti/public/checkName"
+	sdname := sdName{Name: stationName, City: cityName}
+	body := cnRequest{TheName: sdname, MaxList: maxList, MaxDistance: maxDistance}
+
+	body_json, err := json.Marshal(body)
+
+	if err != nil {
+		log.Fatalf("Error while creating JSON for checkName")
+	}
+
+	body_buffer := bytes.NewBuffer(body_json)
+	req, err := http.NewRequest("POST", url, body_buffer)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	signature := getSignature(body_json)
+	fmt.Printf("Signature for checkName request: %s\n", signature)
+
+	return execRequest(client, signature, req)
+}
+
+func requestDepartureList(stationName string, cityName string, maxList int, maxTimeOffset int, client *http.Client) *http.Response {
+	url := "http://gti.geofox.de/gti/public/departureList"
+	sdname := sdName{Name: stationName, City: cityName}
+	body := dlRequest{Station: sdname, MaxList: maxList, MaxTimeOffset: maxTimeOffset}
+
+	body_json, err := json.Marshal(body)
+
+	if err != nil {
+		log.Fatalf("Error while creating JSON for departureList")
+	}
+
+	body_buffer := bytes.NewBuffer(body_json)
+	req, err := http.NewRequest("POST", url, body_buffer)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	signature := getSignature(body_json)
+	fmt.Printf("Signature for departureList request: %s\n", signature)
+
+	return execRequest(client, signature, req)
+}
+
+func main() {
+	fmt.Print("Starting HTTP Client...\n")
+	client := &http.Client{}
+
+	resp := requestCheckName("Altona", "Hamburg", 1, 10, client)
 
 	resp_body, err := io.ReadAll(resp.Body)
 
